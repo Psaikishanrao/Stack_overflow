77ea42a9d17a2cfdf89f88e317f6cd704c5dad11
Psaikishanrao <saikishan785@gmail.com>
2024-11-20 01:58:30 +0530

    header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,32 +2,31 @@ import React from "react";
 import "../styles/header.css";
 import SearchIcon from "@mui/icons-material/Search";
 import InboxIcon from "@mui/icons-material/Inbox";
-import { Avatar } from "@mui/material"; 
+import { Avatar } from "@mui/material";
 
 const Header = () => (
   <header className="header">
-    <div className="main-container">
-      <div className="left">
+    <div className="header-container">
+      <div className="header-left">
         <img
           src="https://stackoverflow.design/assets/img/logos/so/logo-stackoverflow.svg"
           alt="Logo"
+          className="header-logo"
         />
-        <h3>Products</h3>
+        <h3 className="header-products">Products</h3>
       </div>
-
-      <div className="middle">
-        <div className="search">
-          <SearchIcon />
+      <div className="header-middle">
+        <div className="header-search-container">
+          <SearchIcon className="search-icon" />
           <input type="text" placeholder="Search..." />
         </div>
       </div>
-
-      <div className="right">
-        <div className="avatar">
-          <Avatar />
+      <div className="header-right">
+        <div className="header-right-container">
+          <Avatar className="header-avatar" />
           <InboxIcon />
         </div>
-        <button>Login</button>
+        <button className="login-button">Login</button>
       </div>
     </div>
   </header>
